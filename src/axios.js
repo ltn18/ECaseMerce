5977db3c6055ff394e0bb0c3764dc902ed87b2c7
Tùng Trần <tungttran@Tungs-MacBook-Pro.local>
2021-11-26 00:17:03 -0500

    delete firebase

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: "https://us-central1-clone-9ac61.cloudfunctions.net/api"
-           //"http://localhost:5001/clone-9ac61/us-central1/api"  // API - cloud function URL
 });
 
 export default instance;
