f65542ad5841b58c9eed6d7eee5d100b85f4dc77
HOANGLAM2952002 <tommy2952002@gmail.com>
2021-11-26 01:22:47 -0500

    handle search, restyle carousel, and fix logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,60 +1,82 @@
-import React from 'react';
-import '../css/Header.css';
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
-import { Link, useHistory } from 'react-router-dom';
-import { useStateValue } from '../StateProvider';
-import SearchIcon from '@material-ui/icons/Search';
+import React, { useState } from "react";
+import "../css/Header.css";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link, useHistory } from "react-router-dom";
+import { useStateValue } from "../StateProvider";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Header = () => {
+  const [{ basket, user }, dispatch] = useStateValue();
+  const [searchInput, setSearchInput] = useState("");
 
-    const[{basket, user}, dispatch] = useStateValue();
+  const history = useHistory();
 
-    const history = useHistory();
-
-    const handleAuthentication = () => {
-        if (user) {
-            //auth.signOut();
-        }
+  const handleAuthentication = () => {
+    if (user) {
+      //auth.signOut();
     }
+  };
 
-    return (
-        <div className="header">
-            <Link to="/">
-                <img className="header__logo" 
-                    src="ECaseMerce.jpg"></img>
-            </Link>
-            <div
-                className="header__search"
-            >
-                <input className="header__searchInput" type="text"></input>    
-                <SearchIcon className="header__searchIcon"></SearchIcon>
-            </div> 
+  const handleSearch = () => {
+    if (searchInput !== "") console.log(searchInput);
+  };
 
-            <div className="header__nav">
-                <div className="header__option">
-                    <span className="header__flag"></span>
-                </div>
-                <Link className="text-link" to={!user && "/login"} >
-                <div onClick={handleAuthentication} className="header__option">
-                    <span className="header__optionLineOne">{user ? "Welcome, " + user?.email.substring(0, user?.email.indexOf('@')) : "Welcome, guest"}</span>
-                    <span className="header__optionLineTwo">{user ? "Sign out" : "Sign in"}</span>
-                </div>
-                </Link>
-                <Link className="text-link" to="/orders">
-                <div className="header__option">
-                    <span className="header__optionLineOne">Returns</span>
-                    <span className="header__optionLineTwo">& Orders</span>
-                </div>
-                </Link>
-                <Link className="text-link" to="/checkout">
-                    <div className="header__optionBasket">
-                        <ShoppingBasketIcon />
-                        <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
-                    </div>
-                </Link>
-            </div>
+  return (
+    <div className="header">
+      <Link to="/">
+        <img className="header__logo" src="ECaseMerce_edit.jpg"></img>
+      </Link>
+      <div className="header__search">
+        <input
+          className="header__searchInput"
+          type="text"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") handleSearch();
+          }}
+        ></input>
+        <SearchIcon
+          className="header__searchIcon"
+          onClick={handleSearch}
+          style={{ cursor: "pointer" }}
+        ></SearchIcon>
+      </div>
+
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__flag"></span>
         </div>
-    );
+        <Link className="text-link" to={!user && "/login"}>
+          <div onClick={handleAuthentication} className="header__option">
+            <span className="header__optionLineOne">
+              {user
+                ? "Welcome, " +
+                  user?.email.substring(0, user?.email.indexOf("@"))
+                : "Welcome, guest"}
+            </span>
+            <span className="header__optionLineTwo">
+              {user ? "Sign out" : "Sign in"}
+            </span>
+          </div>
+        </Link>
+        <Link className="text-link" to="/orders">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+        <Link className="text-link" to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,125 +1,112 @@
-import React from 'react';
-import Product from '../components/Product';
-import Carousel from 'react-bootstrap/Carousel';
+import React from "react";
+import Product from "../components/Product";
+import Carousel from "react-bootstrap/Carousel";
 import "../css/Home.css";
 
 const Home = () => {
-    
-    return (
-        <div className="home">
-            <div className = "home__container">
-                
-            <Carousel>
-                <Carousel.Item interval={1000}>
-                    <img
-                    className="d-block w-100"
-                    src="https://images-na.ssl-images-amazon.com/images/G/01/AMAZON_FASHION/2021/journeys/MGY0YzMzNTMt/MGY0YzMzNTMt-NTM1Y2E3OTYt-w1500._CB411425260_.jpg"
-                    alt="Gameplay deal"
-                    />
-                    <Carousel.Caption>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={1000}>
-                    <img
-                    className="d-block w-100"
-                    src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/ZjNlMmIxN2Et/ZjNlMmIxN2Et-N2JkNGVlMzct-w1500._CB661992256_.jpg"
-                    alt="Favourite items"
-                    />
-                    <Carousel.Caption>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval = {1000}>
-                    <img
-                    className="d-block w-100"
-                    src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/Nzg3NzIxZDct/Nzg3NzIxZDct-OGNhZDRmNzYt-w1500._CB412118632_.jpg"
-                    alt="Wifi 6"
-                    />
-                    <Carousel.Caption>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+  return (
+    <div className="home">
+      <div className="home__container">
+        <Carousel>
+          <Carousel.Item interval={1000}>
+            <img
+              className="d-block w-100"
+              src="https://dailymedia.case.edu/wp-content/uploads/2018/08/22085723/class-2022-cwru-feat.jpg"
+              alt="Favourite items"
+            />
+            <Carousel.Caption></Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={1000}>
+            <img
+              className="d-block w-100"
+              src="https://dailymedia.case.edu/wp-content/uploads/2016/12/15184212/ksl-oval.jpg"
+              alt="Wifi 6"
+            />
+            <Carousel.Caption></Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
 
-                <div className="home__row">
-                    <Product 
-                        id = "12345"
-                        title="Beats Solo3 Wireless On-Ear Headphones - Apple W1 Headphone Chip, Class 1 Bluetooth, 40 Hours Of Listening Time - Black (Latest Model)" 
-                        price={229.99}  
-                        image="https://images-na.ssl-images-amazon.com/images/I/51QxA-98Q%2BL._AC_SX342_.jpg"
-                        rating={5}
-                    ></Product>
+        <div className="home__row">
+          <Product
+            id="12345"
+            title="Beats Solo3 Wireless On-Ear Headphones - Apple W1 Headphone Chip, Class 1 Bluetooth, 40 Hours Of Listening Time - Black (Latest Model)"
+            price={229.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51QxA-98Q%2BL._AC_SX342_.jpg"
+            rating={5}
+          ></Product>
 
-                    <Product
-                        id = "23456"
-                        title="Sony WF-1000XM3/B Bluetooth Noise Cancellation Wireless In-Ear Headphones With Mic and Voice Control, Black" 
-                        price={179.69}  
-                        image="https://images-na.ssl-images-amazon.com/images/I/61zKkP36kDL._AC_SL1500_.jpg"
-                        rating={4}
-                    ></Product>
+          <Product
+            id="23456"
+            title="Sony WF-1000XM3/B Bluetooth Noise Cancellation Wireless In-Ear Headphones With Mic and Voice Control, Black"
+            price={179.69}
+            image="https://images-na.ssl-images-amazon.com/images/I/61zKkP36kDL._AC_SL1500_.jpg"
+            rating={4}
+          ></Product>
 
-                    <Product
-                        id = "23654"
-                        title="Sony SRS-XB33 EXTRA BASS Wireless Portable Speaker" 
-                        price={99.99}  
-                        image="https://m.media-amazon.com/images/I/81NfNapLYxL._AC_UY218_.jpg"
-                        rating={4}
-                    ></Product>
-                </div>
-
-                <div className="home__row">
-                    <Product
-                        id = "67890"
-                        title="Fitbit Charge 4 Special Edition Fitness and Activity Tracker with Built-in GPS, Heart Rate, Sleep & Swim Tracking, Black/Granite Reflective, One Size (S &L Bands Included)" 
-                        price={154.50}  
-                        image="https://images-na.ssl-images-amazon.com/images/I/81i8wx2ObIL._AC_SX425_.jpg"
-                        rating={4}
-                    ></Product>
-
-                    <Product
-                        id = "65670"
-                        title="All-new Echo Dot (4th Gen, 2020 release) | Smart speaker with Alexa | Charcoal" 
-                        price={49.99}  
-                        image="https://m.media-amazon.com/images/I/714Rq4k05UL._AC_UY218_.jpg"
-                        rating={4}
-                    ></Product>
-                </div>
-
-                <div className="home__row">
-                    <Product
-                        id = "34547"
-                        title="Apple iPhone 11 [128GB, Purple] + Carrier Subscription [Cricket Wireless]" 
-                        price={699.99}  
-                        image="https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/2132584444.jpeg"
-                        rating={5}
-                    ></Product>
-
-                    <Product
-                        id = "45448"
-                        title="New Apple iPad Pro (12.9-inch, Wi-Fi, 256GB) - Space Gray (4th Generation)" 
-                        price={999.00}  
-                        image="https://m.media-amazon.com/images/I/81SGb5l+lZL._AC_UY218_.jpg"
-                        rating={5}
-                    ></Product> 
-                    <Product
-                        id = "56589"
-                        title="New Apple MacBook Pro with Apple M1 Chip (13-inch, 8GB RAM, 512GB SSD Storage) - Silver (Latest Model)" 
-                        price={1399.00}  
-                        image="https://m.media-amazon.com/images/I/71gD8WdSlaL._AC_UY218_.jpg"
-                        rating={5}
-                    ></Product>
-                </div>
-
-                <div className="home__row">
-                    <Product
-                        id = "65690"
-                        title="SAMSUNG LC34J791WTNXZA 34-Inch CJ791 Ultrawide Curved Gaming Monitor, White" 
-                        price={679.99}  
-                        image="https://m.media-amazon.com/images/I/91-hWcNu2yL._AC_UY218_.jpg"
-                        rating={5}
-                    ></Product>
-                </div>
-            </div>
+          <Product
+            id="23654"
+            title="Sony SRS-XB33 EXTRA BASS Wireless Portable Speaker"
+            price={99.99}
+            image="https://m.media-amazon.com/images/I/81NfNapLYxL._AC_UY218_.jpg"
+            rating={4}
+          ></Product>
         </div>
-    );
+
+        <div className="home__row">
+          <Product
+            id="67890"
+            title="Fitbit Charge 4 Special Edition Fitness and Activity Tracker with Built-in GPS, Heart Rate, Sleep & Swim Tracking, Black/Granite Reflective, One Size (S &L Bands Included)"
+            price={154.5}
+            image="https://images-na.ssl-images-amazon.com/images/I/81i8wx2ObIL._AC_SX425_.jpg"
+            rating={4}
+          ></Product>
+
+          <Product
+            id="65670"
+            title="All-new Echo Dot (4th Gen, 2020 release) | Smart speaker with Alexa | Charcoal"
+            price={49.99}
+            image="https://m.media-amazon.com/images/I/714Rq4k05UL._AC_UY218_.jpg"
+            rating={4}
+          ></Product>
+        </div>
+
+        <div className="home__row">
+          <Product
+            id="34547"
+            title="Apple iPhone 11 [128GB, Purple] + Carrier Subscription [Cricket Wireless]"
+            price={699.99}
+            image="https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/2132584444.jpeg"
+            rating={5}
+          ></Product>
+
+          <Product
+            id="45448"
+            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 256GB) - Space Gray (4th Generation)"
+            price={999.0}
+            image="https://m.media-amazon.com/images/I/81SGb5l+lZL._AC_UY218_.jpg"
+            rating={5}
+          ></Product>
+          <Product
+            id="56589"
+            title="New Apple MacBook Pro with Apple M1 Chip (13-inch, 8GB RAM, 512GB SSD Storage) - Silver (Latest Model)"
+            price={1399.0}
+            image="https://m.media-amazon.com/images/I/71gD8WdSlaL._AC_UY218_.jpg"
+            rating={5}
+          ></Product>
+        </div>
+
+        <div className="home__row">
+          <Product
+            id="65690"
+            title="SAMSUNG LC34J791WTNXZA 34-Inch CJ791 Ultrawide Curved Gaming Monitor, White"
+            price={679.99}
+            image="https://m.media-amazon.com/images/I/91-hWcNu2yL._AC_UY218_.jpg"
+            rating={5}
+          ></Product>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
